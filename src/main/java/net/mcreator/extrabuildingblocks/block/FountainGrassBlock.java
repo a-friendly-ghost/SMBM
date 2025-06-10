783f79a70b5babc832bbfdc7f292b4be7a0300e0
a-friendly-ghost <mishanandgov@gmail.com>
2025-06-10 17:23:29 +0100

    fixed mining tool for cactus wood and chorus wood blocks, and added fountain grass valid placement condition

--- src/main/java/net/mcreator/extrabuildingblocks/block/FountainGrassBlock.java
+++ src/main/java/net/mcreator/extrabuildingblocks/block/FountainGrassBlock.java
@@ -7,9 +7,15 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
+
+import net.mcreator.extrabuildingblocks.procedures.FountainGrassBlockValidPlacementConditionProcedure;
 
 public class FountainGrassBlock extends Block {
 	public FountainGrassBlock() {
@@ -29,5 +35,21 @@ public class FountainGrassBlock extends Block {
 	@Override
 	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
 		return Shapes.empty();
+	}
+
+	@Override
+	public boolean canSurvive(BlockState blockstate, LevelReader worldIn, BlockPos pos) {
+		if (worldIn instanceof LevelAccessor world) {
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			return FountainGrassBlockValidPlacementConditionProcedure.execute(world, x, y, z);
+		}
+		return super.canSurvive(blockstate, worldIn, pos);
+	}
+
+	@Override
+	public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor world, BlockPos currentPos, BlockPos facingPos) {
+		return !state.canSurvive(world, currentPos) ? Blocks.AIR.defaultBlockState() : super.updateShape(state, facing, facingState, world, currentPos, facingPos);
 	}
 }
---
+++ src/main/java/net/mcreator/extrabuildingblocks/procedures/FountainGrassBlockValidPlacementConditionProcedure.java
@@ -0,0 +1,15 @@
+package net.mcreator.extrabuildingblocks.procedures;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.BlockPos;
+
+public class FountainGrassBlockValidPlacementConditionProcedure {
+	public static boolean execute(LevelAccessor world, double x, double y, double z) {
+		if ((world.getBlockState(BlockPos.containing(x, y - 1, z))).is(BlockTags.create(new ResourceLocation("minecraft:dry_vegetation_may_place_on")))) {
+			return true;
+		}
+		return false;
+	}
+}
